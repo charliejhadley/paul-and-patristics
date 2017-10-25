abd1adac6ebe42eb18cc77ddac5ebcbb5444a8ab
martinjhnhadley <martinjohnhadley@gmail.com>
2017-10-25 16:01:50 +0100

    tidied ui.R by removing old features

---
+++ ui.R
@@ -0,0 +1,32 @@
+library("highcharter")
+
+navbarPage(
+  "Paul and Patristics",
+  tabPanel(
+    "Database",
+    uiOutput("region_filter_UI"),
+    DT::dataTableOutput("patristics_table") 
+  ),
+  tabPanel(
+    "Epistle Usage Visualisations",
+    fluidPage(
+      
+      selectInput("epistle_usage_selected_viz",
+                  "Break down usage by:",
+                  choices = c("Century",
+                              "Early Christian Author",
+                              "Region")),
+      uiOutput("epistle_usage_viz")
+    )
+  ),
+  tabPanel("Early Christian Writer Visualisations",
+           uiOutput("writer_viz_selected_author_UI"),
+           # highchartOutput("writer_viz_texts")
+           uiOutput("writer_viz_hc_UI")
+           # uiOutput("writer_viz_ggplot2_UI")
+           )
+  
+  
+  
+  
+)
